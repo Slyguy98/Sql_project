8f9c5e7741b31065be13b2223e329a0677e5185c
Slyguy98 <Jamilsalman85@gmail.com>
2026-02-24 15:19:51 -0600

    hotfix

--- tables/player_metrics.sql
+++ tables/player_metrics.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS player_metrics CASCADE;
 
 CREATE TABLE player_metrics (
     metric_id SERIAL PRIMARY KEY,
-    -- This MUST be appid to match the games table
     appid INT REFERENCES games(appid) ON DELETE CASCADE, 
     current_players INT NOT NULL,
     peak_24h INT,
