d68b3a5ed9229e6d8e295a07bf97bae44f12eb24
Slyguy98 <Jamilsalman85@gmail.com>
2026-03-09 18:35:11 -0500

    authetification key update test

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -89,7 +89,7 @@ with tab4:
     
     fig_bar = px.bar(
         top_10_approval, 
-        x='approval_rating', 
+        x='approval_ratin',
         y='genre', 
         orientation='h',
         title="Top 10 Rated Genres (Min. 50 Games)",
